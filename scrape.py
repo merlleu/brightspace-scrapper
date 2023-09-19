9c4c830ad0568377ef4aaccc1c25b15e4d129314
Mr e-RL <r.langdorph@gmail.com>
2023-09-19 12:56:44 +0200

    added multithreading

--- scrape.py
+++ scrape.py
@@ -1,23 +1,22 @@
 from brightspace_scrapper import Scrapper
 from brightspace_scrapper.content import extract_content, sanitize
+from concurrent.futures import ThreadPoolExecutor
 import json, os
+from tqdm import tqdm
 
 scrapper = Scrapper()
 courses = scrapper.get_courses()
 
-
-for course in courses:
-    print(course)
-
+def process_course(course):
     r = scrapper.get_course_content(course['id'])
     cname = sanitize(course['name'])
     sem = sanitize(course['semester'])
     os.makedirs(f"courses/{sem}/{cname}", exist_ok=True)
     with open(f"courses/{sem}/{cname}/content.json", "w") as f:
         json.dump(r, f, indent=4)
-    
-    for content in extract_content(r):
-        print(content)
+    contentlist = extract_content(r)
+
+    for content in tqdm(contentlist, desc=f"Processing course {course['id']}", leave=False):
         ext = content['url'].split('.')[-1]
         path = "/".join(content['path'])
         filepath = f"courses/{sem}/{cname}/{path}/{content['name']}.{ext}"
@@ -29,5 +28,9 @@ for course in courses:
 
         if len(filepath) > 215:
             filepath = filepath[:210] + "." + ext
+        
         with open(filepath, "wb") as f:
             f.write(r)
+
+with ThreadPoolExecutor(max_workers=10) as executor:
+    executor.map(process_course, courses)
